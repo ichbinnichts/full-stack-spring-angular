0e3ca954ccda0516929cc936694f6c39f7a844fe
Nathan Víctor de Faria <nathan.faria@inatel.br>
2022-07-12 17:11:17 -0300

    feat: add a new exception

--- src/main/java/com/example/demo/model/repo/EmployeeRepository.java
+++ src/main/java/com/example/demo/model/repo/EmployeeRepository.java
@@ -1,10 +1,18 @@
 package com.example.demo.model.repo;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.model.Employee;
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+    @Query("DELETE e FROM Employee e WHERE e.id = ?1")
+    void deleteEmployeeById(Long id);
+
+    @Query("SELECT e FROM Employee e WHERE e.id = ?1")
+    Optional<Employee> findEmployeeById(Long id);
     
 }
--- src/main/java/com/example/demo/model/service/EmployeeService.java
+++ src/main/java/com/example/demo/model/service/EmployeeService.java
@@ -1,13 +1,35 @@
 package com.example.demo.model.service;
 
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.stereotype.Service;
 
+import com.example.demo.exceptions.UserNotFoundException;
+import com.example.demo.model.Employee;
 import com.example.demo.model.repo.EmployeeRepository;
+
 @Service
 public class EmployeeService {
     private final EmployeeRepository employeeRepository;
-    
+
     public EmployeeService(EmployeeRepository employeeRepository) {
         this.employeeRepository = employeeRepository;
+    }
+    public Employee addEmployee(Employee employee){
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepository.save(employee);
+    }
+    public List<Employee> findAllEmployees(){
+        return employeeRepository.findAll();
+    }
+    public Employee updateEmployee(Employee employee){
+        return employeeRepository.save(employee);
+    }
+    public Employee findEmployeeById(Long id){
+        return employeeRepository.findEmployeeById(id).orElseThrow(() -> new UserNotFoundException("Employee with id: "+ id + " does not exist"));
+    }
+    public void deleteEmployee(Long id){
+        employeeRepository.deleteEmployeeById(id);
     }
 }
